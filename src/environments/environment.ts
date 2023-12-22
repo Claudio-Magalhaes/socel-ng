9fbc31481d38853cdb7c0c2088f5753207d5e0dc
Claudio Magalhaes <claudio.magalhaes@datagrupo.com.br>
2023-12-21 21:32:01 -0300

    teste

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,7 +5,8 @@
 export const environment = {
   production: false,
   apiUrl: 'http://localhost:3001/api/',
-  apiUrl_mock: 'http://localhost:3000/'
+  apiUrl_mock: 'http://localhost:3000/',
+  teste: ''
 };
 
 /*
